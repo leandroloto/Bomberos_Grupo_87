32c91b14614b863d766043c04a416fa51950eaa0
PamPam7327 <141685293+PamPam7327@users.noreply.github.com>
2023-10-02 20:47:06 -0300

    clase siniestro

---
+++ src/bomberos_grupo_87/Entidades/Siniestro.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bomberos_grupo_87.Entidades;
+
+/**
+ *
+ * @author psg_7
+ */
+public class Siniestro {
+    
+}
