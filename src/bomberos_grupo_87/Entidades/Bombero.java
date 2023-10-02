ea8673e375bf90b2b4120a1e1ab20ca5c6a9bc6f
GabrielGelves <gabrieltombino84@gmail.com>
2023-10-02 20:51:14 -0300

    update 2-10

--- src/bomberos_grupo_87/Entidades/Bombero.java
+++ src/bomberos_grupo_87/Entidades/Bombero.java
@@ -1,14 +1,98 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bomberos_grupo_87.Entidades;
 
-/**
- *
- * @author Discar
- */
+import java.time.LocalDate;
+
+
 public class Bombero {
+    private int codBombero;
+    private int dni;
+    private String nombre_ape;
+    private LocalDate fecha_nac;
+    private String grupo_sang;
+    private String celular;
+    private Brigada codBrigada;
+    private boolean estado;
+
+    public Bombero(int codBombero, int dni, String nombre_ape, LocalDate fecha_nac, String grupo_sang, String celular, Brigada codBrigada, boolean estado) {
+        this.codBombero = codBombero;
+        this.dni = dni;
+        this.nombre_ape = nombre_ape;
+        this.fecha_nac = fecha_nac;
+        this.grupo_sang = grupo_sang;
+        this.celular = celular;
+        this.codBrigada = codBrigada;
+        this.estado = estado;
+    }
+
+    public int getCodBombero() {
+        return codBombero;
+    }
+
+    public void setCodBombero(int codBombero) {
+        this.codBombero = codBombero;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre_ape() {
+        return nombre_ape;
+    }
+
+    public void setNombre_ape(String nombre_ape) {
+        this.nombre_ape = nombre_ape;
+    }
+
+    public LocalDate getFecha_nac() {
+        return fecha_nac;
+    }
+
+    public void setFecha_nac(LocalDate fecha_nac) {
+        this.fecha_nac = fecha_nac;
+    }
+
+    public String getGrupo_sang() {
+        return grupo_sang;
+    }
+
+    public void setGrupo_sang(String grupo_sang) {
+        this.grupo_sang = grupo_sang;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public Brigada getCodBrigada() {
+        return codBrigada;
+    }
+
+    public void setCodBrigada(Brigada codBrigada) {
+        this.codBrigada = codBrigada;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "DNI: "+ dni + "- Nombre y Apellido: "+ nombre_ape + ", Grupo Sanguineo: " + grupo_sang + ", Brigada:" + codBrigada;
+    }
+   
     
 }
---
+++ src/bomberos_grupo_87/Entidades/Brigada.java
@@ -0,0 +1,67 @@
+
+package bomberos_grupo_87.Entidades;
+
+
+public class Brigada {
+    
+    private int codBrigada;
+    private String nombre_br;
+    private String especialidad;
+    private boolean libre;
+    private Cuartel codCuartel;
+
+    public Brigada(int codBrigada, String nombre_br, String especialidad, boolean libre, Cuartel codCuartel) {
+        this.codBrigada = codBrigada;
+        this.nombre_br = nombre_br;
+        this.especialidad = especialidad;
+        this.libre = libre;
+        this.codCuartel = codCuartel;
+    }
+
+    public int getCodBrigada() {
+        return codBrigada;
+    }
+
+    public void setCodBrigada(int codBrigada) {
+        this.codBrigada = codBrigada;
+    }
+
+    public String getNombre_br() {
+        return nombre_br;
+    }
+
+    public void setNombre_br(String nombre_br) {
+        this.nombre_br = nombre_br;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public boolean isLibre() {
+        return libre;
+    }
+
+    public void setLibre(boolean libre) {
+        this.libre = libre;
+    }
+
+    public Cuartel getCodCuartel() {
+        return codCuartel;
+    }
+
+    public void setCodCuartel(Cuartel codCuartel) {
+        this.codCuartel = codCuartel;
+    }
+
+    @Override
+    public String toString() {
+        return "Brigada" + codBrigada + ", Nombre: " + nombre_br + ", Especialidad: " + especialidad;
+    }
+    
+    
+}
