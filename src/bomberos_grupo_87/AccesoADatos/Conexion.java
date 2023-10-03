654c109fcd18322f9fb4ac483088d8ec47eec775
GabrielGelves <gabrieltombino84@gmail.com>
2023-10-02 21:00:52 -0300

    conexion

--- src/bomberos_grupo_87/AccesoADatos/Conexion.java
+++ src/bomberos_grupo_87/AccesoADatos/Conexion.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package bomberos_grupo_87.AccesoADatos;
 
-/**
- *
- * @author Discar
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
 public class Conexion {
+    private static final String URL="jdbc:mariadb://localhost/";
+    private static final String DB="bomberos_grupo_87";
+    private static final String USUARIO="root";
+    private static final String PASSWORD="";
+    private static Connection connection;
+    
+    private Conexion(){
+        
+    }
+    
+    public static Connection getConexion(){
+        if(connection == null){
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection = DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);
+                JOptionPane.showMessageDialog(null,"Conexion Exitosa");
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null,"Error en la carga de Driver "+ ex.getMessage());
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null,"Error en la conexion con la base de datos "+ ex.getMessage());
+            }
+            
+        }
+        return connection;
+    }
     
 }
