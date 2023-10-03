4d548e51816b060e4ad5bd6378210eb2821e0489
PamPam7327 <141685293+PamPam7327@users.noreply.github.com>
2023-10-03 18:32:36 -0300

    actualizacion siniestro-entidades

--- src/bomberos_grupo_87/Entidades/Siniestro.java
+++ src/bomberos_grupo_87/Entidades/Siniestro.java
@@ -5,10 +5,106 @@
  */
 package bomberos_grupo_87.Entidades;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author psg_7
  */
 public class Siniestro {
+  
+    private int codSiniestro;
+    private String tipo;
+    private LocalDate fecha_siniestro;
+    private int coord_X;
+    private int coord_Y;
+    private String detalles;
+    private LocalDate fecha_resol;
+    private int puntuacion;
+    private int codBrig;
+
+    public Siniestro( String tipo, LocalDate fecha_siniestro, int coord_X, int coord_Y, String detalles, LocalDate fecha_resol, int puntuacion, int codBrig) {
+        this.tipo = tipo;
+        this.fecha_siniestro = fecha_siniestro;
+        this.coord_X = coord_X;
+        this.coord_Y = coord_Y;
+        this.detalles = detalles;
+        this.fecha_resol = fecha_resol;
+        this.puntuacion = puntuacion;
+        this.codBrig = codBrig;
+    }
+
+    public int getCodSiniestro() {
+        return codSiniestro;
+    }
+
+    public void setCodSiniestro(int codSiniestro) {
+        this.codSiniestro = codSiniestro;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDate getFecha_siniestro() {
+        return fecha_siniestro;
+    }
+
+    public void setFecha_siniestro(LocalDate fecha_siniestro) {
+        this.fecha_siniestro = fecha_siniestro;
+    }
+
+    public int getCoord_X() {
+        return coord_X;
+    }
+
+    public void setCoord_X(int coord_X) {
+        this.coord_X = coord_X;
+    }
+
+    public int getCoord_Y() {
+        return coord_Y;
+    }
+
+    public void setCoord_Y(int coord_Y) {
+        this.coord_Y = coord_Y;
+    }
+
+    public String getDetalles() {
+        return detalles;
+    }
+
+    public void setDetalles(String detalles) {
+        this.detalles = detalles;
+    }
+
+    public LocalDate getFecha_resol() {
+        return fecha_resol;
+    }
+
+    public void setFecha_resol(LocalDate fecha_resol) {
+        this.fecha_resol = fecha_resol;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    public int getCodBrig() {
+        return codBrig;
+    }
+
+    public void setCodBrig(int codBrig) {
+        this.codBrig = codBrig;
+    }
+    
     
 }
